a7336fc7d56eec385ed31f8bcee363c559272207
Kevin CHERI-ZECOTE <kev.cheriz@gmail.com>
2020-08-04 18:11:13 +0200

    debug

--- src/Controller/EntrepriseController.php
+++ src/Controller/EntrepriseController.php
@@ -2,47 +2,103 @@
 
 namespace App\Controller;
 
-use App\Form\EntrepriseType;
 use App\Entity\Entreprise;
+use App\Form\Entreprise1Type;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-//use Symfony\Component\Security\Core\Encoder\EntreprisePasswordEncoderInterface;
 
+/**
+ * @Route("/entreprise")
+ */
 class EntrepriseController extends AbstractController
 {
-     /**
+    /**
+     * @Route("/", name="entreprise_index", methods={"GET"})
+     */
+    public function index(): Response
+    {
+        $entreprises = $this->getDoctrine()
+            ->getRepository(Entreprise::class)
+            ->findAll();
+
+        return $this->render('entreprise/index.html.twig', [
+            'entreprises' => $entreprises,
+        ]);
+    }
+
+      /**
      * @Route("/profile/entreprise", name="profile_entreprise")
      */
-    public function index(){
-        return $this->render('Entreprise/index.html.twig');
+    public function back_entreprise(){
+        return $this->render('Entreprise/entreprise_back.html.twig');
     }
-//    /**
-//     * @Route("/register", name="entreprise_registration")
-//     */
-//    public function register(Request $request/*,EntreprisePasswordEncoderInterface $passwordEncoder*/)
-//    {
-//
-//        $entreprise = new Entreprise();
-//        $form = $this->createForm(EntrepriseType::class, $entreprise);
-//
-//        $form->handleRequest($request);
-//        if ($form->isSubmitted() && $form->isValid()) {
-//            $entityManager = $this->getDoctrine()->getManager();
-//            //$password = $passwordEncoder->encodePassword($entreprise, $entreprise->getPlainPassword());
-//            $entreprise->setPassword(base64_encode($entreprise->getPlainPassword()));
-//            $entityManager->persist($entreprise);
-//            $entityManager->flush();
-//
-//            return $this->redirectToRoute('entreprise_registration');
-//        }
-//
-//        return $this->render(
-//            'entreprise/register.html.twig',
-//            array(
-//                'form' => $form->createView(),
-//                'entreprise' => $entreprise
-//                )
-//        );
-//    }
+
+    /**
+     * @Route("/new", name="entreprise_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $entreprise = new Entreprise();
+        $form = $this->createForm(Entreprise1Type::class, $entreprise);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($entreprise);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('entreprise_index');
+        }
+
+        return $this->render('entreprise/new.html.twig', [
+            'entreprise' => $entreprise,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="entreprise_show", methods={"GET"})
+     */
+    public function show(Entreprise $entreprise): Response
+    {
+        return $this->render('entreprise/show.html.twig', [
+            'entreprise' => $entreprise,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="entreprise_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Entreprise $entreprise): Response
+    {
+        $form = $this->createForm(Entreprise1Type::class, $entreprise);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('entreprise_index');
+        }
+
+        return $this->render('entreprise/edit.html.twig', [
+            'entreprise' => $entreprise,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="entreprise_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Entreprise $entreprise): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$entreprise->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($entreprise);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('entreprise_index');
+    }
 }
